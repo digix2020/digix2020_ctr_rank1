5fc2e561cb9ad6329f84ac4c6816f40f9a2f3b52
dada-dada <1315885735@qq.com>
2020-11-18 13:36:48 +0800

    Update fusion.py

--- ctr/fusion.py
+++ ctr/fusion.py
@@ -1,9 +1,9 @@
 import pandas as pd
 import numpy as np
 
-res1 = pd.read_csv('submission_4_all.csv')
-res2 = pd.read_csv('submission_4fold_win.csv')
-res3 = pd.read_csv('submission_4_all_nounique.csv')
+res1 = pd.read_csv('result/submission_4_all.csv')
+res2 = pd.read_csv('result/submission_4fold_win.csv')
+res3 = pd.read_csv('result/submission_4_all_nounique.csv')
 
 res1['probability'] = res1['probability'] * 0.6 + res2['probability'] * 0.2 + res3['probability'] * 0.2
 res1['probability'] = res1['probability'].astype(np.float32)
